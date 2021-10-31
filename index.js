bce82fae994130f8563f47e790388dabccb7c1fa
sakil72642 <mission.scic.04@gmail.com>
2021-10-31 19:43:59 +0600

    final

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ app.use(cors())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9clk0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-
-
 async function run() {
     try {
         await client.connect();
@@ -82,8 +79,6 @@ async function run() {
             console.log(result);
             res.json(result)
         })
-
-
 
 
     } finally {
