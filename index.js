0d4f7bb1f634e5c90004e90291542a13c8604401
sakil-sagor <sakilsagor76543@gmail.com>
2021-11-15 23:33:28 +0600

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 app.use(express.json())
 
 const port = process.env.PORT || 5000
-// middleware
+// middleware 
 var cors = require('cors')
 app.use(cors())
 
@@ -71,7 +71,7 @@ async function run() {
             res.json(result)
         })
 
-        // orders delete 
+        // orders delete code
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
